70026347ea6e62d429a89936979b98bc444ea524
xxefi <magsudluefgan@gmail.com>
2025-02-02 04:26:09 +0400

    author1

--- BooksWithNewModel/front/app/services/bookService.ts
+++ BooksWithNewModel/front/app/services/bookService.ts
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-//export const API_URL = process.env.NEXT_PUBLIC_API_URL;
-export const API_URL = "http://localhost:3000/api/books";
+export const API_URL = process.env.NEXT_PUBLIC_API_URL;
 
 const handleApiError = (e: unknown) => {
   if (axios.isAxiosError(e)) {
